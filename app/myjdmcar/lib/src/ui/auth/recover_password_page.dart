508b074a0a53b16b186842807f649bf1256d74f1
JB2DEV <javier.alca.2000@gmail.com>
2021-05-11 15:53:26 +0200

    Maquetación Página Recuperar Contraseña

--- app/myjdmcar/lib/src/ui/auth/recover_password_page.dart
+++ app/myjdmcar/lib/src/ui/auth/recover_password_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:myjdmcar/src/widgets/buttons/theme_button.dart';
+import 'package:myjdmcar/src/widgets/decoration/logo_app.dart';
+import 'package:myjdmcar/src/widgets/form/textformfields/email_textformfield.dart';
 
 class RecoverPasswordPage extends StatefulWidget {
   @override
@@ -6,10 +9,52 @@ class RecoverPasswordPage extends StatefulWidget {
 }
 
 class _RecoverPasswordPageState extends State<RecoverPasswordPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
-    );
+        body: GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 30),
+              LogoApp(),
+              SizedBox(height: 50),
+              Text("Ayúdanos a recuperar tu contraseña"),
+              SizedBox(height: 30),
+              Text("Introduce tu correo y te enviaremos un correo con las instrucciones"),
+              SizedBox(height: 30),
+              Form(key: _formKey, child: EmailTextFormField()),
+              SizedBox(height: 30),
+              SizedBox(
+                height: 80,
+              ),
+              ThemeButton(function: validateForm, buttonText: "Recover"),
+              SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
+
+  ///Valida el que el formulario cumpla las restricciones
+  void validateForm() {
+    if (_formKey.currentState.validate()) {
+      print("validated");
+      Navigator.pop(context);
+    } else {
+      print("Not validated");
+    }
   }
 }
